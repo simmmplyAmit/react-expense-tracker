72b92f08896b5f4fa7279fce11cb50391434edde
Amit Vyas <simmmplyamit@gmail.com>
2020-06-29 14:05:43 +0530

    Updates

---
+++ precache-manifest.ad195b463ab1f1636df2ad2e277a95fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9b69b4da63e811dca0165b0b10b0bd",
+    "url": "/react-expense-tracker/index.html"
+  },
+  {
+    "revision": "b287b5503916398bafe0",
+    "url": "/react-expense-tracker/static/css/2.407546cd.chunk.css"
+  },
+  {
+    "revision": "d0024d4bff771001ec28",
+    "url": "/react-expense-tracker/static/css/main.208bfe5d.chunk.css"
+  },
+  {
+    "revision": "b287b5503916398bafe0",
+    "url": "/react-expense-tracker/static/js/2.aa87a8fb.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/react-expense-tracker/static/js/2.aa87a8fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0024d4bff771001ec28",
+    "url": "/react-expense-tracker/static/js/main.68e66cce.chunk.js"
+  },
+  {
+    "revision": "7edc2b17c6c41405d05a",
+    "url": "/react-expense-tracker/static/js/runtime-main.2e9adeb0.js"
+  }
+]);
